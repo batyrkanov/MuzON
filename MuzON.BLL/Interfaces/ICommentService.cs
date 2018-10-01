2bf54e9ab78c79827ea3e1aca9588d1b0ddeb687
batyrkanov <artez994@gmail.com>
2018-10-01 17:36:33 +0600

    added mapping profile and first service

---
+++ MuzON.BLL/Interfaces/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using MuzON.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzON.BLL.Interfaces
+{
+    public interface ICommentService
+    {
+        IEnumerable<CommentDTO> GetComments();
+        IEnumerable<CommentDTO> GetCommentsBySongId(Guid Id);
+        IEnumerable<CommentDTO> GetCommentsByUserId(Guid Id);
+        void AddComment(CommentDTO commentDTO);
+        void Dispose();
+    }
+}
---
+++ MuzON.BLL/MappingProfiles/BLLMapingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MuzON.BLL.DTO;
+using MuzON.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzON.BLL.MappingProfiles
+{
+    public class BLLMapingProfile : Profile
+    {
+        public BLLMapingProfile()
+        {
+            CreateMap<Artist, ArtistDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Band, BandDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Song, SongDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Country, CountryDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Comment, CommentDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Rating, RatingDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<Playlist, PlaylistDTO>(MemberList.None).ReverseMap();
+
+            CreateMap<BandSong, BandSongDTO>(MemberList.None).ReverseMap();
+        }
+    }
+}
